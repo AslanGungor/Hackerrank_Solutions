0cb3f787e0d2a91018ac77d94729f9c78dbebc19
aslan53 <aslangungor5571@hotmail.com>
2019-10-17 13:50:56 +0300

    Proje Başlatıldı

---
+++ 1_hackerrank.py
@@ -0,0 +1,13 @@
+n=int(input("Lütfen sayıyı giriniz"))
+m=int(3*int(n))
+print(n,m)
+for i in range(1,n+1):
+    if i==(n+1)/2:
+        for m in range(1,m+1):  
+            print(".",end="")
+    else:
+        for k in range(1,m+1):
+            print("-",end="")
+            if k==(m-1)/2:              #En orta sütuna "|" işareti koyuyor
+                print("|",end="")
+    print()                             #Satır Atlama
